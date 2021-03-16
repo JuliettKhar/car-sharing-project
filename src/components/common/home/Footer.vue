7a01411f78c80e6dc6295fa15cfe431fc7611a39
julia <juliettkhar@gmail.com>
2021-03-16 23:21:08 +0300

    add main section

---
+++ src/components/common/home/Footer.vue
@@ -0,0 +1,29 @@
+<template>
+  <el-footer class="footer">
+    <a href="#" class="footer__copyrights">&#169; 2016-2019 «Need for drive»</a>
+    <a href="tel:+74952342244" class="footer__phone">8 (495) 234-22-44</a>
+  </el-footer>
+</template>
+
+<script>
+  export default {
+    name: "Footer",
+  };
+</script>
+
+<style scoped lang="scss">
+  .footer {
+    display: flex;
+    justify-content: space-between;
+
+    &__copyrights {
+      font-size: 13px;
+      color: $gray;
+    }
+
+    &__phone {
+      font-size: 13px;
+      color: $black;
+    }
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,18 @@
 <template>
   <section class="home">
     <div class="home__main">
-      <el-header></el-header>
-      <div class="home__main-content">main</div>
-      <el-footer></el-footer>
+      <Header />
+      <div class="home__main-content">
+        <div>
+          <p class="">Каршеринг</p>
+          <p>Need for drive</p>
+          <p>Поминутная аренда авто твоего города</p>
+          <el-button class="home__main-content__button"
+            >Забронировать</el-button
+          >
+        </div>
+      </div>
+      <Footer />
     </div>
     <div class="home__slider">
       slider
@@ -12,9 +21,11 @@
 </template>
 
 <script>
+  import Header from "@/components/common/home/Header";
+  import Footer from "@/components/common/home/Footer";
   export default {
-    name: 'Home',
-    components: {},
+    name: "Home",
+    components: { Header, Footer },
   };
 </script>
 <style lang="scss">
@@ -25,6 +36,7 @@
     &__main {
       display: flex;
       flex-direction: column;
+      padding: 32px 64px;
       width: 50%;
       max-width: 50%;
     }
@@ -33,7 +45,48 @@
       display: flex;
       flex: 1;
       align-items: center;
-      justify-content: center;
+
+      & > div {
+        text-align: left;
+      }
+
+      & > div p {
+        margin: 0;
+
+        &:first-child {
+          font-weight: bold;
+          font-size: 70px;
+          color: $black;
+        }
+
+        &:nth-child(2) {
+          margin: 0 0 30px 0;
+          font-weight: bold;
+          font-size: 70px;
+          color: $green;
+        }
+
+        &:nth-child(3) {
+          margin: 0 0 60px 0;
+          font-size: 26px;
+          color: $gray;
+        }
+      }
+    }
+
+    &__main-content__button {
+      width: 250px;
+      background: linear-gradient(
+        90deg,
+        $green 2.61%,
+        $dark-green 112.6%
+      ) !important;
+      background-blend-mode: darken;
+      border-radius: 8px !important;
+      color: $white !important;
+      font-weight: 500 !important;
+      font-size: 18px !important;
+      line-height: 21px !important;
     }
 
     &__slider {
