47634f3d2056079b52258f9b59906a1c4dd48f23
julia <juliettkhar@gmail.com>
2021-04-02 21:19:19 +0300

    Updates

--- @/assets/js/Confirm.d416c844.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["Confirm"],{b20c:function(n,r,e){"use strict";e.r(r);var c=function(){var n=this,r=n.$createElement,e=n._self._c||r;return e("div",{staticClass:"confirm-order"},[n._v(" Confirm ")])},t=[],i={name:"ConfirmOrder"},o=i,a=e("2877"),s=Object(a["a"])(o,c,t,!1,null,"1cc7a7f0",null);r["default"]=s.exports}}]);
-//# sourceMappingURL=Confirm.d416c844.js.map
